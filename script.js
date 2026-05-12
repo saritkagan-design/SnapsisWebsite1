83f695e65a0db7140259ec1b408342a2494e069e
saritkagan-design <saritkagan@gmail.com>
2026-05-12 16:57:27 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+function showMessage(){
+    document.getElementById("message").innerHTML =
+    "This technology could help Alzheimer's patients feel safer and more connected to their families.";
+}
